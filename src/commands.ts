9fff432611c24a3ab9eaa20f9846a746e042e54c
Muhamad Rizki Subagya <78222169+mrsubagya@users.noreply.github.com>
2025-01-01 01:14:28 +0700

    Add files via upload

--- src/commands.ts
+++ src/commands.ts
@@ -1,10 +1,16 @@
 import { CommandSay } from "./commands/CommadSay";
 import Command from "./commands/Command";
 import { CommandMenu } from "./commands/CommandMenu";
+import { CommandBuff } from "./commands/CommandBuff";
+import { CommandLeveling } from "./commands/CommandLeveling";
+import { CommandNuklir } from "./commands/CommandNuklir";
 
 const commands: Command[] = [
     new CommandSay(),
-    new CommandMenu()
+    new CommandMenu(),
+    new CommandBuff(),
+    new CommandLeveling(),
+    new CommandNuklir()
 ];
 
 export default commands
